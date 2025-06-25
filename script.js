f93fbb6cd31775cfc274e0586da93d2f2bc0f51e
Maurice Stemmert <mauricestemmert@MacBook-Pro-von-Maurice.local>
2025-06-25 19:05:23 +0200

    Rumprobieren, wie etwas durchgeht

--- script.js
+++ script.js
@@ -5,17 +5,56 @@ let promError = false;
   function getPromise() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            console.log("1");
+            
             if(promError){
                 reject("Hat nicht geklappt");
             }
             else{
                 resolve("Hat geklappt");
             }
-        }, 300);
+        }, 1000);
+      });
+  }
+  function getPromise2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("2");
+            if(!promError){
+                reject("Hat nicht geklappt");
+            }
+            else{
+                resolve("Hat geklappt");
+            }
+        }, 100);
       });
   }
 
-function usePromise() {
-    let prom = getPromise();
-    console.log(prom);
+  function getPromise3() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("3");
+            if(promError){
+                reject("Hat nicht geklappt");
+            }
+            else{
+                resolve("Hat geklappt");
+            }
+        }, 1000);
+      });
+  }
+
+async function usePromise() {
+    let prom = "nothing happened";
+    try {
+    await getPromise();
+    await getPromise2();
+    await getPromise3();
+    console.log("juhu");
 }
+    catch (error) {
+        console.log(error);
+    }
+    console.log("ende");
+}
+    
